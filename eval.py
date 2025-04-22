6c9d536b75df1b9ea9d0744cc5befcf674e3eb47
Udder <pirate-era@hotmail.com>
2025-04-22 22:33:46 +0200

    Touch ups

--- eval.py
+++ eval.py
@@ -57,24 +57,3 @@ if __name__ == "__main__":
 
     print(f"Macro Accuracy: {macro_accuracy:.2f}")
     print(f"Micro Accuracy: {micro_accuracy:.2f}")
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="NLI Evaluation task")
-    parser.add_argument("--seed", type=int, default=1234)
-    parser.add_argument("--encoder_model", type=str, default="mean", help="Choices would be, uni-lstm, bi-lstm, bi-max-lstm, mean")
-    parser.add_argument("--epochs", type=int, default=20) # It converges based on the LR so best to let it run for a while with a higher value
-    parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--dropout_encoder", type=float, default=0.0)
-    parser.add_argument("--dropout_classifier", type=float, default=0.0)
-    parser.add_argument("--lstm_dim", type=int, default=512)
-    parser.add_argument("--classifier_dim", type=int, default=512)
-    parser.add_argument("--class_count", type=int, default=3)
-    parser.add_argument("--sgd_lr", type=float, default=0.1)
-    parser.add_argument("--sgd_momentum", type=float, default=0.9)
-    parser.add_argument("--decay", type=float, default=0.99)
-    # Paths
-    parser.add_argument("--outputdir", type=str, default='modelsaves/')
-    parser.add_argument("--checkpoint_path", type=str, default='modelsaves/mean_model.pth')
-    parser.add_argument("--outputmodelname", type=str, default='model_output')
-    parser.add_argument("--glovepath", type=str, default='glove.840B.300d.txt')
-    parser.add_argument("--sentevalpath", type=str, default='../SentEval')
